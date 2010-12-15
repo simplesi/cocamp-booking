023301f12d639b292f3e98547f43bdd8ea793914
simon <simon@simplesi.org>
2010-12-15 00:49:56 +0000

    Switch email to cocamp noreply address

--- src/uk/org/woodcraft/bookings/email/EmailUtils.java
+++ src/uk/org/woodcraft/bookings/email/EmailUtils.java
@@ -25,8 +25,9 @@ public class EmailUtils {
             Message msg = new MimeMessage(session);
             
             // FIXME: Change to proper admin address / Event name
-            msg.setFrom(new InternetAddress("simon@simplesi.org", "CoCamp Bookings Admin"));
+            msg.setFrom(new InternetAddress("no-reply@cocamp.coop", "CoCamp Bookings Admin"));
             msg.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail(), user.getName()));
+            msg.addRecipient(Message.RecipientType.BCC, new InternetAddress("cocamp-email-archive@woodcraft.org.uk", user.getName()));
             msg.setSubject(subject);
             msg.setText(body);
             Transport.send(msg);
