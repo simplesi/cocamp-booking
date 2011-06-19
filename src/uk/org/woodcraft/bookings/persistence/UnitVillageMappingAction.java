c19a475014612b7522c20b0317a9b9c6dfb401f1
simon <simon@simplesi.org>
2011-06-19 11:07:29 +0100

    Village mappings complete

--- src/uk/org/woodcraft/bookings/persistence/UnitVillageMappingAction.java
+++ src/uk/org/woodcraft/bookings/persistence/UnitVillageMappingAction.java
@@ -6,10 +6,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import uk.org.woodcraft.bookings.auth.SecurityModel;
+import uk.org.woodcraft.bookings.auth.SessionConstants;
 import uk.org.woodcraft.bookings.datamodel.Booking;
 import uk.org.woodcraft.bookings.datamodel.Unit;
-import uk.org.woodcraft.bookings.datamodel.UnitVillageMapping;
 import uk.org.woodcraft.bookings.datamodel.Village;
+import uk.org.woodcraft.bookings.utils.SessionUtils;
 
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
@@ -18,13 +19,9 @@ public class UnitVillageMappingAction extends SessionBasedAction{
 
 	private static final long serialVersionUID = 1L;
 	
-	private Collection<UnitVillageMapping> mappings = new ArrayList<UnitVillageMapping>();
 	private Map<String, String> map = new HashMap<String,String>();
-	private Collection<Booking> problemBookings;
+	private Collection<Booking> problemBookings = new ArrayList<Booking>();
 
-	public Collection<UnitVillageMapping> getMappings() {
-		return mappings;
-	}
 	
 	public Map<String, String> getMap() {
 		return map;
@@ -36,11 +33,16 @@ public class UnitVillageMappingAction extends SessionBasedAction{
 	
 	private void populateMappings() 
 	{
-		mappings.clear();
+		map.clear();
 		for(Unit unit : CannedQueries.allUnits(false))
 		{
-			mappings.add(new UnitVillageMapping(unit, unit.getVillageKey()));
+			map.put(unit.getWebKey(), unit.getVillageWebKey());
 		}
+	}
+	
+	public Collection<Unit> getUnits()
+	{
+		return CannedQueries.allUnits(false);
 	}
 	
 	public String list()
@@ -54,43 +56,46 @@ public class UnitVillageMappingAction extends SessionBasedAction{
 	public String applyMappings()
 	{
 		SecurityModel.checkIsAdminUser(this);
-		populateMappings();
 		
 		int unitsChanged = 0;
 		int bookingsChanged = 0;
 		
 		problemBookings = new ArrayList<Booking>();
 		
-		for(UnitVillageMapping mapping : mappings)
+		for(Unit unit : getUnits())
 		{
-			String proposedNewVillageKey = map.get(mapping.getUnitWebKey());
+			String proposedNewVillageKey = map.get(unit.getWebKey());
 			if (proposedNewVillageKey == null || proposedNewVillageKey.equals("")) continue;
 			
 			Key newKey = KeyFactory.stringToKey(proposedNewVillageKey);
 			
-			if (mapping.getVillageKey() == null || !mapping.getVillageKey().equals(newKey))
-			{	
-				Unit unit = mapping.getUnit();				
+			if (unit.getVillageKey() == null || !unit.getVillageKey().equals(newKey))
+			{			
 				Collection<Booking> bookings = CannedQueries.bookingsForUnit(unit, getCurrentEvent());
 				Collection<Booking> bookingsToUpdate = new ArrayList<Booking>();
+				
 				for(Booking booking : bookings)
 				{
 					// Don't touch those where the village key was not the old village key for the unit 
 					// (ie that had already been altered manually)
 					if (booking.getVillageKey() == null 
-						|| booking.getVillageKey().equals(mapping.getVillageKey()))
+						|| booking.getVillageKey().equals(unit.getVillageKey()))
 					{
-						booking.setVillageKey(mapping.getVillageKey());
+						booking.setVillageKey(newKey);
 						bookingsToUpdate.add(booking);
 						
 					} else {
 						getProblemBookings().add(booking);
 					}
 				}
+				
 				unit.setVillageKey(newKey);
 				
 				CannedQueries.save(bookingsToUpdate);
 				CannedQueries.save(unit);
+				
+				// Ensure session has the right unit mapping too
+				SessionUtils.syncSessionCacheIfRequired(getSession(), SessionConstants.CURRENT_UNIT, unit);
 				
 				unitsChanged++;
 				bookingsChanged += bookingsToUpdate.size();
--- war/templates/datamodel/unitVillageMapping.ftl
+++ war/templates/datamodel/unitVillageMapping.ftl
@@ -1,10 +1,33 @@
 <#assign pagetitle="Unit-Village Assignment">
 <#include "/templates/header.ftl">
 <h2>Village Assignments</h2>
-<p>To change the village for a given unit, please alter it in the dropdown below, and press apply. 
-Bookings which were already manually changed to a different unit will not be affected.</p>
 <@s.actionmessage/>
 <@s.actionerror/>
+
+<#if ( ProblemBookings.size() != 0)>
+<p>Not all bookings were switched to new villages, as some already had a custom village set. If you need to change these bookings, they should be altered directly</p>
+<h3>Unchanged bookings</h3>
+<table>
+   <tr>
+   		<th>Unit</th>
+        <th>Booking Name</th>
+        <th>Current village</th>
+    </tr>
+    
+    <#list ProblemBookings as booking>
+    	<@s.url id="editURL" action="editBooking" webKey="${booking.webKey}" />
+        <tr>
+        	<td>${booking.unit.name}</td>
+            <td> <@s.a href="${editURL}">${booking.name!"unnamed"}</@s.a><#if booking.isCancelled> (Cancelled)</#if></td>
+       		<td>${booking.village.name!}</td>
+        </tr>
+    </#list>
+</table>
+</#if>
+
+<p>To change the village for a given unit, please alter it in the dropdown below, and press apply. 
+Bookings which were already manually changed to a different unit will not be affected.</p>
+
 
 <@s.form action="applyUnitVillageMappings" method="post">
 <table>
@@ -13,11 +36,11 @@ Bookings which were already manually changed to a different unit will not be aff
         <th>Village</th>
     </tr>
     
-    <#list Mappings as mapping>
+    <#list Units as unit>
         <tr>
-            <td>${mapping.unitName!"unnamed unit"}</td>
+            <td>${unit.name!"unnamed unit"}</td>
             <td>
-				<@s.select name="map['${mapping.unitWebKey}']" list="villages" label="" listKey="webKey" listValue="name" value=mapping.VillageWebKey labelposition="left" labelSeparator=""/>		
+				<@s.select name="map['${unit.webKey}']" list="villages" label="" listKey="webKey" listValue="name" labelposition="left" labelSeparator=""/>		
 			</td>
         </tr>
     </#list>
